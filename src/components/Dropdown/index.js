dc9d0da138a4140ea198a4b7e6f76ea43440abea
Marcus Ang <marcus.ang@tookitaki.com>
2018-12-13 10:51:12 +0800

    Added support to pass any props to Dropdown component

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -2,67 +2,19 @@ import React from 'react';
 import Select from 'antd/lib/select';
 const Option = Select.Option;
 
-const Dropdown = ({ items, placeholder, handleChange, defaultValue, labelKey, valueKey }) => {
-  if (items && Array.isArray(items) && items.length > 0) {
-    let dropdownOptions = items.map((item, idx) => {
-      return (
-        <Option key={idx} value={valueKey && item[valueKey] ? item[valueKey] : item}>
-          {labelKey && item[labelKey] ? item[labelKey] : item}
-        </Option>
-      );
-    });
+const Dropdown = (props) => {
+  return (
+    <Select {...props} />
+  );
+}
 
-    return (
-      <Select
-        style={{ width: '100%' }}
-        placeholder={placeholder || 'Please select an item'}
-        defaultValue={defaultValue}
-        onChange={handleChange}>
-        { dropdownOptions }
-      </Select>
-    );
-  }
-
+const MultiDropdown = (props) => {
   return (
     <Select
-      style={{ width: '100%' }}
-      placeholder={placeholder || 'No items available'}
-      defaultValue="disabled">
-      <Option value="disabled" disabled>No items available</Option>
+      {...props}
+      mode="multiple">
     </Select>
   );
 }
 
-const MultiDropdown = ({ items, placeholder, handleChange, defaultValue, valueKey, labelKey }) => {
-  if (items && Array.isArray(items) && items.length > 0) {
-    let dropdownOptions = items.map((item, idx) => {
-      return (
-        <Option key={idx} value={valueKey && item[valueKey] ? item[valueKey] : item}>
-          {labelKey && item[labelKey] ? item[labelKey] : item}
-        </Option>
-      );
-    });
-
-    return (
-      <Select
-        style={{ width: '100%' }}
-        mode="multiple"
-        defaultValue={defaultValue}
-        placeholder={placeholder || 'Please select an item'}
-        onChange={handleChange}>
-        { dropdownOptions }
-      </Select>
-    );
-  }
-
-  return (
-    <Select
-      style={{ width: '100%' }}
-      placeholder={placeholder || 'No items available'}
-      defaultValue="disabled">
-      <Option value="disabled" disabled>No items available</Option>
-    </Select>
-  );
-}
-
-export { Dropdown, MultiDropdown }
+export { Dropdown, MultiDropdown, Option }
