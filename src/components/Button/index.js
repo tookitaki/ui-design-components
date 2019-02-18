5be3ee2af07165be746c176e8f3c03966a652515
Marcus Ang <marcus.ang@tookitaki.com>
2019-02-18 16:47:05 +0800

    Added tertiary button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import style from './index.scss';
 
-export const Button = (props) => {
+export const Button = props => {
   return (
     <button {...props} className={style.TTButtonPrimary}>{props.children}</button>
   );
 }
 
-export const ButtonSecondary = (props) => {
+export const ButtonSecondary = props => {
   return (
     <button {...props} className={style.TTButtonSecondary}>{props.children}</button>
+  );
+}
+
+export const ButtonTertiary = props => {
+  return (
+    <button {...props} className={style.TTButtonTertiary}>{props.children}</button>
   );
 }
