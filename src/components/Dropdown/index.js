59300879dc7d2dfd47dfaf88fad942df5546ec29
Marcus Ang <marcus.ang@tookitaki.com>
2019-03-26 15:45:07 +0800

    [PD-7] Added style to Dropdown component

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Select from 'antd/lib/select';
+
+import style from './index.scss';
+
 const Option = Select.Option;
 
 const Dropdown = (props) => {
   return (
-    <Select style={{ width: '100%' }} {...props} />
+    <Select className={style.TTDropdown} style={{ width: '100%' }} {...props} />
   );
 }
 
